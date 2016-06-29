f0f75549e2275420a57c907c1808d7cd9a7bab37
Subhajit Sahu <wolfram77@gmail.com>
2016-06-29 14:55:19 +0530

    program: dev & test

--- cs-char/Program.cs
+++ cs-char/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace cs_char {
 	class Program {
+
+		/// <summary>
+		/// Harness lightning.
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			oParams p = GetOpt(args);
+			TextReader cin = p.input == null ? Console.In : new StringReader(p.input);
+			if (p.ascii) ToAscii(cin, p.verbose);
+			else ToChar(cin, p.verbose);
+		}
+
+		/// <summary>
+		/// Output characters of input ASCII codes.
+		/// </summary>
+		/// <param name="cin">Input stream.</param>
+		/// <param name="vb">Verbose?</param>
+		private static void ToChar(TextReader cin, bool vb) {
+			char[] c = new char[256];
+			while(true) {
+				int v = 0;
+				string ln = cin.ReadLine();
+				if (ln == null) return;
+				if (!int.TryParse(ln, out v) || v < 0 || v > 255) { Console.Write("<ascii? {0}>{1}", ln, vb? "\n" : ""); continue; }
+				if (vb) Console.WriteLine("{0} : {1}", ln, (char)v);
+				else Console.Write((char)v);
+			}
+		}
+
+		/// <summary>
+		/// Output ASCII codes of input characters.
+		/// </summary>
+		/// <param name="cin">Input stream.</param>
+		/// <param name="vb">Verbose?</param>
+		private static void ToAscii(TextReader cin, bool vb) {
+			char[] c = new char[256];
+			while(true) {
+				int R = cin.Read(c, 0, c.Length);
+				if (R <= 0) return;
+				for (int r = 0; r < R; r++) {
+					if (vb) Console.WriteLine("{0} : {1}", c[r] < ' ' ? '.' : c[r], (int)c[r]);
+					else Console.WriteLine((int)c[r]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Get input options to ochar.
+		/// </summary>
+		/// <returns>Input options.</returns>
+		private static oParams GetOpt(string[] args) {
+			oParams o = new oParams();
+			for(int i=0; i<args.Length; i++) {
+				switch(args[i]) {
+					case "-a":
+					case "--ascii":
+						o.ascii = true;
+						break;
+					case "-v":
+					case "--verbose":
+						o.verbose = true;
+						break;
+					case "-i":
+					case "--input":
+						o.input = args[++i];
+						break;
+				}
+			}
+			return o;
 		}
 	}
 }
---
+++ cs-char/oParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cs_char {
+	class oParams {
+
+		// data
+		/// <summary>
+		/// Indicates that the ASCII value of input is required.
+		/// </summary>
+		public bool ascii;
+		/// <summary>
+		/// Indicates that both the input and output must be shown, side by side.
+		/// </summary>
+		public bool verbose;
+		/// <summary>
+		/// Defines input string that is to be using instead of stdin.
+		/// </summary>
+		public string input;
+	}
+}
