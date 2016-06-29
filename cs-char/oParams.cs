62657a2cebc871fd40317e2ef9a729b7cdb820f4
Subhajit Sahu <wolfram77@gmail.com>
2016-06-29 15:38:05 +0530

    project: basic proto program created

--- cs-char/oParams.cs
+++ cs-char/oParams.cs
@@ -7,14 +7,14 @@ namespace cs_char {
 		/// <summary>
 		/// Indicates that the ASCII value of input is required.
 		/// </summary>
-		bool ascii;
+		public bool ascii;
 		/// <summary>
 		/// Indicates that both the input and output must be shown, side by side.
 		/// </summary>
-		bool verbose;
+		public bool verbose;
 		/// <summary>
 		/// Defines input string that is to be using instead of stdin.
 		/// </summary>
-		string input;
+		public string input;
 	}
 }
